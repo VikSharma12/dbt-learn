623ba34dbbdc511ed00c3687c60db6c74bbcccf2
vikrant.sharms@gmail.com <vikrant.sharms@gmail.com>
2021-05-31 21:34:56 +0000

    data test for stg payments model

---
+++ tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,7 @@
+select 
+    order_id,
+    sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+--group by the first column in select statement, better to use column name but for now using 1 to see how it works
+group by 1 
+having not(total_amount >= 0)
